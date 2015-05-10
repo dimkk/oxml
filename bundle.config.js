37b68c1a97afef64caf89dc2a2613e7b09d16e57
Dima <dimkk@outlook.com>
2015-05-10 16:17:54 +0300

    order fixed

--- bundle.config.js
+++ bundle.config.js
@@ -5,7 +5,15 @@ module.exports = {
     bundle: {
         oxml: {
             scripts: [
-                './src/**/*js'
+                'src/linq.js',
+                'src/ltxml.js',
+                'src/ltxml-extensions.js',
+                'src/jszip.js',
+                'src/jszip-load.js',
+                'src/jszip-inflate.js',
+                'src/jszip-deflate.js',
+                'src/fileSaver.js',
+                'src/openxml.js'
             ],
             options: {
                 uglify: true,
